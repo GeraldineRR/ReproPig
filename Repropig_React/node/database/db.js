bc50a348d9bd28747745602f74dff56354d66934
Andry Rubiano <andryrubiano6@gmail.com>
2026-04-28 10:20:43 -0500

    db

--- Repropig_React/node/database/db.js
+++ Repropig_React/node/database/db.js
@@ -1,18 +1,9 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD, // 🔥 AQUÍ VA LA PASS
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
-    logging: false,
-  }
-);
+const db = new Sequelize("repropig", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: false,
+});
 
 export default db;
