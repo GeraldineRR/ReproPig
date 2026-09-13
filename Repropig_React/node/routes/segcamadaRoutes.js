990f126fdd8d2a535d7c69bbf35afea8f5150862
david <davidher15141@gmail.com>
2026-09-13 13:59:05 -0500

    Corregir crash de nodemon por imports faltantes

--- Repropig_React/node/routes/segcamadaRoutes.js
+++ Repropig_React/node/routes/segcamadaRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { getAllSegcamadas, getSegcamada, createSegcamada, updateSegcamada, deleteSegcamada, getSegCamadaByCria, getSegCamadaByPorcino, getNotificacionesSeguimiento} from '../controllers/segcamadaController.js';
+import { getAllSegcamadas, getSegcamada, createSegcamada, updateSegcamada, deleteSegcamada, getSegCamadaByCria} from '../controllers/segcamadaController.js';
 
 const router = express.Router();
 
-router.get('/notificaciones', getNotificacionesSeguimiento);
 router.get('/', getAllSegcamadas);
 router.get('/cria/:idCria', getSegCamadaByCria);
 router.get('/:id', getSegcamada);
